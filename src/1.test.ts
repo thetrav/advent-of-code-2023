a51e4b9b9ce90d3b73015db794475a6cb737578c
Travis Dixon <the.trav@gmail.com>
2023-12-03 10:25:44 +1100

    day one part two

--- src/1.test.ts
+++ src/1.test.ts
@@ -1,11 +1,32 @@
-import {describe, expect, test} from '@jest/globals'
-import {parse} from './1'
+import { describe, expect, test } from '@jest/globals'
+import { parse } from './1'
 
-describe('samples', () => {
-  test('from description', () => {
-    expect(parse("1abc2")).toBe(12)
-    expect(parse("pqr3stu8vwx")).toBe(38)
-    expect(parse("a1b2c3d4e5f")).toBe(15)
-    expect(parse("treb7uchet")).toBe(77)
+function t(i: string, n: number) {
+  test(`${i} == ${n}`, () => {
+    expect(parse(i)).toBe(n)
   })
+}
+
+function testCases(cases: [string, number][]) {
+  for (let c of cases) {
+    t(c[0], c[1])
+  }
+}
+
+describe('day one', () => {
+  testCases([
+    ['1abc2', 12],
+    ['pqr3stu8vwx', 38],
+    ['a1b2c3d4e5f', 15],
+    ['treb7uchet', 77],
+  ])
+  testCases([
+    ['two1nine', 29],
+    ['eightwothree', 83],
+    ['abcone2threexyz', 13],
+    ['xtwone3four', 24],
+    ['4nineeightseven2', 42],
+    ['zoneight234', 14],
+    ['7pqrstsixteen', 76],
+  ])
 })
--- src/1.ts
+++ src/1.ts
@@ -1,34 +1,74 @@
 import fs from 'node:fs'
 
-const digits = ['0','1','2','3','4','5','6','7','8','9']
-function isDigit(c: string):boolean {
-    for (let d of digits) {
-        if (c === d) {
-            return true
-        }
+interface IMap {
+  [key: string]: string
+}
+
+const digits: IMap = {
+  '0': '0',
+  '1': '1',
+  '2': '2',
+  '3': '3',
+  '4': '4',
+  '5': '5',
+  '6': '6',
+  '7': '7',
+  '8': '8',
+  '9': '9',
+  zero: '0',
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+  ten: '10',
+  eleven: '11',
+  twelve: '12',
+  thirteen: '13',
+  fourteen: '14',
+  fifteen: '15',
+  sixteen: '16',
+  seventeen: '17',
+  eighteen: '18',
+  nineteen: '19',
+  twenty: '20',
+}
+function digit(index: number, s: string): string | null {
+  for (let k in digits) {
+    if (s[index] === k) {
+      return `${digits[k]}`
     }
-    return false
+    if (s.substring(index).startsWith(k)) {
+      return `${digits[k]}`
+    }
+  }
+  return null
 }
 
 export function parse(s: string): number {
-    var first = ''
-    var last = ''
-    for (let c of s) {
-        if(isDigit(c)) {
-            if (first === '') {
-                first = c
-            }
-            last = c
-        }
+  var first = ''
+  var last = ''
+  for (let i = 0; i < s.length; i++) {
+    const d = digit(i, s)
+    if (d != null) {
+      if (first === '') {
+        first = d[0]
+      }
+      last = d[d.length - 1]
     }
-    return parseInt(first + last)
+  }
+  return parseInt(first + last)
 }
 
 export function runFile(fileName: string): number {
-    const data = fs.readFileSync(fileName, 'utf8');
-    let sum = 0
-    for (let line of data.split("\n")) {
-        sum += parse(line)
-    }
-    return sum
+  const data = fs.readFileSync(fileName, 'utf8')
+  let sum = 0
+  for (let line of data.split('\n')) {
+    sum += parse(line)
+  }
+  return sum
 }
